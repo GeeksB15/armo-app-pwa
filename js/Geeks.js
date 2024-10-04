db7a3627245947f91cc5a946101e6d7a4748adfc
Marcelo Floriano <marcelo@floriano.com.br>
2024-10-04 03:38:06 +0000

    -

--- js/Geeks.js
+++ js/Geeks.js
@@ -66,3 +66,64 @@ var Geeks = {
     });
     return result;
 }
+
+Geeks.Plat.Sql = function (xSql){
+	var url = "https://api.b15.com.br/sql/srv5/ExecuteSQL"
+    var requestDatabase = "GeeksPlat15a"
+    var result = null;
+    var data = xSql;
+	
+	if (!$.isArray(xSql)) {
+		data = xSql.replace(/'/g, "$--$").replace(/"/g, "-#-#-");
+	} else {
+		var sentence = [];
+
+		for (var sql in xSql) {
+			xSql[sql] = xSql[sql].replace(/'/g, "$--$").replace(/"/g, "-#-#-");
+
+			sentence.push(xSql[sql]);
+		}
+
+		//url = "https://api.b15.com.br/sql/srv114/All"
+		data = JSON.stringify(sentence)
+	}
+    
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {
+            database: requestDatabase,
+            sentence: data,
+			user: Usuario ? Usuario.CodigoUsuario : null,
+			caller: "App"
+        },
+        dataType: "json",
+        async: false,
+        success: function(response) {
+			if (!$.isArray(xSql)) {
+				if (response.HasError) {
+					ShowError(response);
+					return;
+				}
+		   	} else {
+		   		var errors = $.grep(response, function(value) {
+					return value.HasError == true;
+				});
+			
+				if (errors.length > 0) {
+					console.log(errors[0])
+					ShowError(errors[0]);
+					return;
+				}
+			}
+
+            result = response;
+        },
+        error: function(e) {
+            console.log(e.responseText)
+            ShowError(e.responseText);
+            return;
+        }
+    });
+    return result;
+}
