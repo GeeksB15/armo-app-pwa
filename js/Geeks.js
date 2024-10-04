044b5651cdbea80a39cea6a202e7f04e200048a0
Marcelo Floriano <marcelo@floriano.com.br>
2024-10-04 01:08:59 +0000

    -

--- js/Geeks.js
+++ js/Geeks.js
@@ -7,10 +7,8 @@ var Geeks = {
 
 
  Geeks.ExternalSQL.Sql = function (xSql){
-	var url = "https://api.b15.com.br/sql/srv8/ExecuteSQL";
-	//var url = "http://srv8.b15.com.br:81/15a/Core/ExecuteSQL";
-	//var url = "http://sql.facilis.com.br/15A/Core/ExecuteSQL";
-    var requestDatabase = "GeeksPlat15a";
+	var url = "https://api.b15.com.br/sql/srv114"
+    var requestDatabase = "armo"
     var result = null;
     var data = xSql;
 	
@@ -25,9 +23,8 @@ var Geeks = {
 			sentence.push(xSql[sql]);
 		}
 
-		url = "https://api.b15.com.br/sql/srv8/ExecuteAllSQL";
-		//url = "http://sql.facilis.com.br/15A/Core/ExecuteSQL";
-		data = JSON.stringify(sentence);
+		//url = "https://api.b15.com.br/sql/srv114/All"
+		data = JSON.stringify(sentence)
 	}
     
     $.ajax({
@@ -68,78 +65,4 @@ var Geeks = {
         }
     });
     return result;
-};
-
-Geeks.ExternalSQL.AsyncSql = function (xSql, functionToCallBack, functionToError, dontShowLoading){
-	if (!dontShowLoading)
-		kendo.mobile.application.showLoading();
-	
-	var url = "https://api.b15.com.br/sql/srv8/ExecuteSQL";
-    var requestDatabase = "GeeksPlat15a";
-    var data = xSql;
-	
-	if (!$.isArray(xSql)) {
-		data = xSql.replace(/'/g, "$--$").replace(/"/g, "-#-#-");
-	} else {
-		var sentence = [];
-
-		for (var sql in xSql) {
-			xSql[sql] = xSql[sql].replace(/'/g, "$--$").replace(/"/g, "-#-#-");
-
-			sentence.push(xSql[sql]);
-		}
-
-		url = "https://api.b15.com.br/sql/srv8/ExecuteAllSQL";
-		data = JSON.stringify(sentence);
-	}
-    
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: {
-            database: requestDatabase,
-            sentence: data,
-			user: Usuario ? Usuario.CodigoUsuario : null,
-			caller: "App"
-        },
-        dataType: "json",
-        async: true,
-        success: function(response) {			
-			if (!dontShowLoading)
-				kendo.mobile.application.hideLoading();
-			
-			if (!$.isArray(xSql)) {
-				if (response.HasError) {
-					ShowError(response);
-					
-					if (functionToError)
-						functionToError(response);
-				}
-		   	} else {
-		   		var errors = $.grep(response, function(value) {
-					return value.HasError == true;
-				});
-			
-				if (errors.length > 0) {
-					console.log(errors[0])
-					ShowError(errors[0]);
-					
-					if (functionToError)
-						functionToError(response);
-				}
-			}
-
-			functionToCallBack(response);
-        },
-        error: function(e) {
-			if (!dontShowLoading)
-				kendo.mobile.application.hideLoading();
-			
-            console.log(e.responseText)
-            ShowError(e.responseText);
-            
-			if (functionToError)
-				functionToError(e);
-        }
-    });
-};
+}
