218b7023227c3aead4efe61b15533cf132d52e6a
Marcelo Floriano <marcelo@floriano.com.br>
2024-10-04 15:17:41 +0000

    -

--- js/App.js
+++ js/App.js
@@ -71,7 +71,7 @@ objLogin_init = function () {
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('../sw.js')
     .then(registration => {
       console.log('Service Worker registrado com sucesso:', registration)
     })
