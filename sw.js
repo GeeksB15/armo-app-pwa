e441451891c4124ebac438640a8c1fe8b6e5815c
Marcelo Floriano <marcelo@floriano.com.br>
2024-10-04 15:27:57 +0000

    -

--- sw.js
+++ sw.js
@@ -3,14 +3,13 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('app-cache').then(cache => {
         return cache.addAll([
-          '/',
           'armo-app-pwa/',
           'armo-app-pwa/favicon.ico',
           '/index.html',
           'armo-app-pwa/manifest.json',
           'armo-app-pwa/sw.js',
-          '/js/App.js',
-          '/js/Geeks.js'
+          'armo-app-pwa/js/App.js',
+          'armo-app-pwa/js/Geeks.js'
         ]);
       })
     );
